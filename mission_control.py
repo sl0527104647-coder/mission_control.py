19cb5d72159e5853ae8d3482ce811453662507f0
shlomo lesawer <sl0527104647@gmail.com>
2026-06-18 15:05:01 +0300

    add_var_type

--- mission_control.py
+++ mission_control.py
@@ -1,9 +1,13 @@
+# mission 1
 agent_name = "shlomo" 
 mission_code = 4647
-distance_to_target = 100
-distance_to_target = float(distance_to_target)
+distance_to_target = 100.0
 mission_active_status = True
 print(agent_name)
 print(mission_code)
 print(distance_to_target)
 print(mission_active_status) 
+print("agent var type" ,type(agent_name))
+print("mission code var type",type(mission_code))
+print("distance var type" ,type(distance_to_target))
+print("mission var type" ,type(mission_active_status))
