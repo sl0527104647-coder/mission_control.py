fdabe03fde3ed0fa50ed1d204e0ba03c53771137
shlomo lesawer <sl0527104647@gmail.com>
2026-06-18 15:15:57 +0300

    full distance

--- mission_control.py
+++ mission_control.py
@@ -1,13 +1,18 @@
-# mission 1
+# mission 1/2
 agent_name = "shlomo" 
 mission_code = 4647
 distance_to_target = 100.0
 mission_active_status = True
+# mission 3
 print(agent_name)
 print(mission_code)
 print(distance_to_target)
 print(mission_active_status) 
+# mission 4
 print("agent var type" ,type(agent_name))
 print("mission code var type",type(mission_code))
 print("distance var type" ,type(distance_to_target))
 print("mission var type" ,type(mission_active_status))
+# mission 5
+full_distance = distance_to_target*2
+print(full_distance)
