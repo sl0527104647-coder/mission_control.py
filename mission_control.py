8280e65ecc52066222fbcae1b03712bcc4586315
shlomo lesawer <sl0527104647@gmail.com>
2026-06-18 14:38:26 +0300

    Add basic mission data

---
+++ mission_control.py
@@ -0,0 +1,9 @@
+agent_name = "shlomo" 
+mission_code = 4647
+distance_to_target = 100
+distance_to_target = float(distance_to_target)
+mission_active_status = True
+print(agent_name)
+print(mission_code)
+print(distance_to_target)
+print(mission_active_status) 
